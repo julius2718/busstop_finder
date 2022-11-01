459f1ac5113b08f7dc2eb33b1410568461fe58bd
julius2718 <yuhgon0928@gmail.com>
2022-11-01 23:03:10 +0100

    Implement geocoding

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 import geopy as gpy
 
 # Load addresses data
-df_add = pd.read_csv('./data/adresses.csv') #CSVの列名について指定しておく必要あり
+df_add = pd.read_csv('./data/adresses.csv')
 
 # Make complete addresses
 address_format = lambda x: '{}, {}, {}'.format(*x)
@@ -15,9 +15,11 @@ geolocator = gpy.Nominatim(user_agent='julius2718')
 
 def get_coordinates(address: str, geolocator: gpy.Nominatim):
     location = geolocator.geocode(address)
-    return (location.latitude, location.longitude)
+    return (location.longitude, location.latitude)
 
 df_add = df_add.assign(
     Coords=df_add['Adresse complète'].apply(lambda x: get_coordinates(x, geolocator))
 )
-df_add[['Lat', 'Long']] = pd.DataFrame(df_add['Coords'].tolist(), index=df_add.index)
+df_add[['Long', 'Lat']] = pd.DataFrame(df_add['Coords'].tolist(), index=df_add.index)
+
+df_points = gpd.tools.geocode(df_add['Adresse complète'], provider='nominatim', user_agent='julius2718')
